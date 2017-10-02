8833a6eeb83bdbf5c99dc8d7ee9bef569671b0de
gienasd <30533388+gienasd@users.noreply.github.com>
2017-10-02 17:02:54 +0200

    Update MySqlConnector.java

--- src/main/java/pl/teamjava/hotel/models/MySqlConnector.java
+++ src/main/java/pl/teamjava/hotel/models/MySqlConnector.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class MySqlConnector {
     private static final String SQL_LINK = "jdbc:mysql://5.135.218.27:3306/Hotel?useUnicode=true&characterEncoding=UTF-8";
-    private static final String SQL_USER = "PawelP";
-    private static final String SQL_PASS = "coseainmam";
+    private static final String SQL_USER = "";
+    private static final String SQL_PASS = "";
     private static final String SQL_CLASS = "com.mysql.jdbc.Driver";
 
     private static MySqlConnector connector = new MySqlConnector();
